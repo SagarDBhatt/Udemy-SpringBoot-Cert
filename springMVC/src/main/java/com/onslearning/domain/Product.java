82cbb77061cd8d049c701dd1284ebab00f828f4c
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-05-25 16:40:48 -0600

    Create Prodcut.java
    Entity class for product listing on Spring MVC web application

---
+++ springMVC/src/main/java/com/onslearning/domain/Product.java
@@ -0,0 +1,41 @@
+package com.onslearning.domain;
+
+
+public class Product {
+    private Integer id;
+    private String description;
+    private Double price;
+    private String imageURL;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
